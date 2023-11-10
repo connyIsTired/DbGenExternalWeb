4fec049460a8f5673c11f1348111a2dc1a9a1c7f
Conrad Kersten <67552218+connyIsTired@users.noreply.github.com>
2023-11-09 23:32:14 -0600

    Fix typo in app module

--- DatabaseDiagramGenerator/src/app/app.module.ts
+++ DatabaseDiagramGenerator/src/app/app.module.ts
@@ -3,9 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { GeneratorComponent } from './components/generator/generator.component';
-import { GeneratorInputComponent } from './components/Generator/generator-input/generator-input.component';
-import { GeneratorDisplayComponent } from './components/Generator/generator-display/generator-display.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { GeneratorInputComponent } from './components/generator/generator-input/generator-input.component';
+import { GeneratorDisplayComponent } from './components/generator/generator-display/generator-display.component';
 
 @NgModule({
   declarations: [
